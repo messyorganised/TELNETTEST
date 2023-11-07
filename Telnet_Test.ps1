993dd7e57449160fd375f38e961e67850be14cba
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-11-08 09:49:50 +1100

    Add files via upload

---
+++ Telnet_Test.ps1
@@ -0,0 +1,23 @@
+﻿$EmailFrom = "SENDER EMAIL"
+$EmailFromPassword = "SENDER PASSWORD"
+$EmailTo = "RECEIVER EMAIL"
+$Subject = "TEST EMAIL SUBJECT"
+$Body = "
+
+This message indicate that your scan to email configuration is working as per standards. 
+If the configuration does not work on your hardware, please check the hardware and site's configuration.
+"
+
+$SMTPServer = "SMTP SERVER CONFIG"
+$Port = "SMTP SERVER PORT"
+
+
+$EmailPasswordConverted = ConvertTo-SecureString $EmailFromPassword -AsPlainText -Force
+$UserPass = New-Object System.Management.Automation.PSCredential ($Emailfrom, $EmailPasswordConverted)
+
+
+Send-MailMessage -SmtpServer $SMTPServer -Port $port -From $EmailFrom -Credential $UserPass -To $EmailTo -Subject $Subject -Body $Body
+
+
+
+
